e6eca019975e1194cc4b74462582e7eeabc5c251
Rolf Schutten <74961507+rolfschutten@users.noreply.github.com>
2023-05-21 14:14:22 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "fleet" {
+  name     = "rg-${var.ServiceName}-hub-${var.EnvironmentPrefix}-001"
+  location = "${var.Location}"
+}
+
+resource "azurerm_kubernetes_fleet_manager" "fleet" {
+
+  hub_profile {
+    dns_prefix = "${var.ServiceName}"
+  }
+
+  location            = azurerm_resource_group.fleet.location
+  name                = "fleet-${var.ServiceName}-${var.EnvironmentPrefix}-001"
+  resource_group_name = azurerm_resource_group.fleet.name
+}

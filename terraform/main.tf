694cbe68a0ac94fa721ef11a3fe86c1480675528
Rolf Schutten <74961507+rolfschutten@users.noreply.github.com>
2023-05-29 12:29:35 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
+resource "azurerm_resource_group" "fleet" {
+  name     = "rg-${var.ServiceName}-${var.EnvironmentPrefix}-001"
+  location = "${var.Location}"
+}
 
+resource "azurerm_kubernetes_fleet_manager" "fleet" {
+
+  hub_profile {
+    dns_prefix = "${var.ServiceName}"
+  }
+
+  location            = azurerm_resource_group.fleet.location
+  name                = "flhub-${var.ServiceName}-${var.EnvironmentPrefix}-001"
+  resource_group_name = azurerm_resource_group.fleet.name
+}
